67c0f3095b291a509128628ea6f626f5cdb6f808
Ryan Swann <RyanSwann1992@gmail.com>
2017-05-26 17:46:08 +0100

    Added window functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,29 @@
 
 #include <iostream>
+#include <SFML\Graphics.hpp>
+#include <SFML\Window.hpp>
 
 int main()
 {
-	std::cout << "Hello World\n";
-	int i = 0;
-	char c;
-	std::cin >> c;
+	sf::RenderWindow window (sf::VideoMode(300, 300), "SFML_WINDOW", sf::Style::Default);
+
+	while (window.isOpen())
+	{
+		sf::Event e;
+		while (window.pollEvent(e))
+		{
+			switch (e.type)
+			{
+			case sf::Event::Closed:
+			{
+				window.close();
+			}
+			}
+
+			window.clear(sf::Color::Black);
+			window.display();
+		}
+	}
+
 	return 0;
 }
