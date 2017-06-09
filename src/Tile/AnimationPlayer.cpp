eb76f9f74ff83b4fb1dfa7124f4386a5d8cd6c33
Ryan Swann <RyanSwann1992@gmail.com>
2017-06-09 23:11:37 +0100

    Implemented directional animation for entity movement.

--- src/Tile/AnimationPlayer.cpp
+++ src/Tile/AnimationPlayer.cpp
@@ -3,8 +3,10 @@
 #include <Managers\TileSheetManager.h>
 #include <Locators\AnimationDetailsManagerLocator.h>
 #include <Managers\AnimationDetailsManager.h>
+#include <Entities\Direction.h>
 #include "Tile.h"
 #include <assert.h>
+#include <iostream>
 
 AnimationPlayer::Animation::Animation(const std::string & tileSheetName, const std::string & animationName, 
 	int startID, int endID, float frameTime, bool repeatable, const sf::Vector2f & drawLocationSize, bool reversible)
@@ -18,8 +20,14 @@ AnimationPlayer::Animation::Animation(const std::string & tileSheetName, const s
 	m_reversible(reversible),
 	m_frame(),
 	m_currentID(m_startID),
-	m_elaspedTime(0)
+	m_elaspedTime(0),
+	m_animationFinished(false)
 {
+}
+
+const std::string & AnimationPlayer::Animation::getName() const
+{
+	return m_animationName;
 }
 
 sf::IntRect AnimationPlayer::Animation::getDrawLocation(const TileSheet & tileSheet) const
@@ -30,12 +38,37 @@ sf::IntRect AnimationPlayer::Animation::getDrawLocation(const TileSheet & tileSh
 void AnimationPlayer::Animation::update(float deltaTime)
 {
 	m_elaspedTime += deltaTime;
+	
+	if (m_elaspedTime < m_frameTime)
+	{
+		return;
+	}
+
+	if (!m_animationFinished)
+	{
+		m_elaspedTime = 0;
+		if (m_currentID < m_endID)
+		{
+			++m_currentID;
+			if (m_currentID == m_endID)
+			{
+				m_animationFinished = true;
+				return;
+			}
+		}
+	}
+
+	if (m_animationFinished)
+	{
+		reset();
+	}
 }
 
 void AnimationPlayer::Animation::reset()
 {
 	m_currentID = m_startID;
 	m_elaspedTime = 0;
+	m_animationFinished = false;
 }
 
 //Animation Player
@@ -64,11 +97,43 @@ AnimationPlayer::~AnimationPlayer()
 	m_tileSheet->releaseTileSheet();
 }
 
+void AnimationPlayer::play(Direction movementDirection)
+{
+	m_sprite.setScale(1, 1);
+	switch (movementDirection)
+	{
+	case Direction::Up :
+	{
+		play("WalkingUp");
+		break;
+	}
+	case Direction::Down :
+	{
+		play("WalkingDown");
+		break;
+	}
+	case Direction::Right :
+	{
+		play("WalkingRight");
+		break;
+	}
+	case Direction::Left :
+	{
+		play("WalkingLeft");
+		m_sprite.setScale(-1, 1);
+		break;
+	}
+	}
+}
+
 void AnimationPlayer::play(const std::string& animationName)
 {
 	if (m_currentAnimation)
 	{
-		assert(m_currentAnimation->m_animationName != animationName);
+		if (m_currentAnimation->getName() == animationName)
+		{
+			return;
+		}
 		m_currentAnimation->reset();
 	}
 
@@ -88,6 +153,12 @@ void AnimationPlayer::draw(const sf::Vector2f& entityPosition, sf::RenderWindow 
 	m_sprite.setPosition(entityPosition);
 	assert(m_currentAnimation);
 	assert(m_tileSheet);
+	//Hack.
+	//Not sure of work around
+	if (m_sprite.getScale().x - 1)
+	{
+		m_sprite.setPosition(m_sprite.getPosition().x + 16, m_sprite.getPosition().y);
+	}
 	m_sprite.setTextureRect(m_currentAnimation->getDrawLocation(*m_tileSheet));
 	window.draw(m_sprite);
 }
