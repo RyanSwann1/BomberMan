ad6f93c66efe9f8ee479f7b2d2d45e4eec95e641
Ryan Swann <RyanSwann1992@gmail.com>
2017-06-19 14:27:23 +0100

    Implemented ability to destroy crates with explosives.

---
+++ src/Entities/Crate.cpp
@@ -0,0 +1,21 @@
+#include "Crate.h"
+#include "CollisionHandler.h"
+#include <Managers\EntityManager.h>
+
+Crate::Crate(const std::string & name, const sf::Vector2f & position, EntityManager & entityManager, int entityID)
+	: Entity(name, position, entityManager, entityID)
+{
+}
+
+void Crate::update(float deltaTime)
+{
+	CollisionHandler::handleCollisions(m_position, m_entityManager, sf::Vector2f(), *this);
+}
+
+void Crate::handleEntityCollision(const std::unique_ptr<Entity>& entity, const sf::FloatRect & intersection)
+{
+	if (entity->getName() == "Explosion")
+	{
+		m_entityManager.removeEntity(Entity::getID());
+	}
+}
